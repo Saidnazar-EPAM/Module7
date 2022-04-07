eea335872df0363454be394b557a28e482db6bb5
Saidnazar-EPAM <102347678+Saidnazar-EPAM@users.noreply.github.com>
2022-04-07 11:10:17 +0500

    Created

---
+++ Module_7/ConfigurationComponent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_7
+{
+    public class ConfigurationComponent : ConfigurationComponentBase
+    {
+        public ConfigurationComponent(string fileName) : base(fileName)
+        {
+        }
+
+        [ConfigurationItem("C1", ProviderTypes.ConfigurationManager)]
+        public string C1 { get; set; }
+
+        [ConfigurationItem("C2", ProviderTypes.ConfigurationManager)]
+        public string C2 { get; set; }
+
+        [ConfigurationItem("C3", ProviderTypes.ConfigurationManager)]
+        public string C3 { get; set; }
+
+        [ConfigurationItem("C4", ProviderTypes.ConfigurationManager)]
+        public string C4 { get; set; }
+
+        [ConfigurationItem("F1", ProviderTypes.CustomFile)]
+        public string F1 { get; set; }
+
+        [ConfigurationItem("F2", ProviderTypes.CustomFile)]
+        public string F2 { get; set; }
+
+        [ConfigurationItem("F3", ProviderTypes.CustomFile)]
+        public string F3 { get; set; }
+
+        [ConfigurationItem("F4", ProviderTypes.CustomFile)]
+        public string F4 { get; set; }    
+    }
+}
---
+++ Module_7/Program.cs
@@ -0,0 +1,26 @@
+﻿using Module_7;
+
+var saveSetting = new ConfigurationComponent("test.json")
+{
+    C1 = 123.ToString(),
+    C2 = 1.23.ToString(),
+    C3 = "test1",
+    C4 = TimeSpan.FromSeconds(123).ToString(),
+    F1 = 456.ToString(),
+    F2 = 4.56.ToString(),
+    F3 = "test2",
+    F4 = TimeSpan.FromSeconds(456).ToString(),
+};
+saveSetting.SaveSettings();
+
+var loadSetting = new ConfigurationComponent("test.json");
+loadSetting.LoadSettings();
+
+Console.WriteLine(loadSetting.C1);
+Console.WriteLine(loadSetting.C2);
+Console.WriteLine(loadSetting.C3);
+Console.WriteLine(loadSetting.C4);
+Console.WriteLine(loadSetting.F1);
+Console.WriteLine(loadSetting.F2);
+Console.WriteLine(loadSetting.F3);
+Console.WriteLine(loadSetting.F4);
